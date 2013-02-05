493080c4bd7480d728353f739b64cfa82435eb48
Martin Møller <mmj@dbc.dk>
2013-02-05 17:00:57 +0100

    first commit

---
+++ BibdkCartObject.php
@@ -0,0 +1,21 @@
+<?php
+
+class BibdkCartObject {
+
+  /**
+   * @return BibdkCartObject
+   */
+  public static function GetObject() {
+    if(!isset($_SESSION['cartobject'])){
+      BibdkCartObject::SetObject();
+    }
+    return $_SESSION['cartobject'];
+  }
+
+  /**
+   * @return BibdkCartObject
+   */
+  public static function SetObject() {
+    $_SESSION['orderobject'] = new BibdkCartObject();
+  }
+}
