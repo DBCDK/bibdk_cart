e0c6fbf949e2064b7d937350e9d3c325bf420e53
Søren Vibjerg <vibjerg@gmail.com>
2013-02-06 13:55:40 +0100

    BibdkCart updated with crud commands

--- BibdkCart.php
+++ BibdkCart.php
@@ -3,19 +3,53 @@
 class BibdkCart {
 
   /**
-   * @return BibdkCartObject
+   * Add pid to bibdk_cart
+   * @param $pids string|array
    */
-  public static function GetObject() {
-    if(!isset($_SESSION['cartobject'])){
-      BibdkCartObject::SetObject();
-    }
-    return $_SESSION['cartobject'];
+  public static function add ($pids){
+    if (!is_array($pids))
+      $pids = array($pids);
+    foreach($pids as $pid)
+      $_SESSION['bibdk_cart'][$pid] = $pid;
   }
 
   /**
-   * @return BibdkCartObject
+   * Remove pid from cart
+   * @param $pids string|array
    */
-  public static function SetObject() {
-    $_SESSION['orderobject'] = new BibdkCart();
+  public static function remove ($pids){
+
+    if (!is_array($pids))
+      $pids = array($pids);
+
+    foreach($pids as $pid){
+      if (isset($_SESSION['bibdk_cart'][$pid]))
+        unset($_SESSION['bibdk_cart'][$pid]);
+    }
+
   }
+
+  /**
+   * Get all pids in cart
+   * @return array
+   */
+  public static function getAll(){
+    if(!isset($_SESSION['bibdk_cart']))
+      return array();
+    return $_SESSION['bibdk_cart'];
+  }
+
+  /**
+   * Check if pid is in cart
+   * @param $pid
+   * @return bool
+   */
+  public static function checkInCart($pid){
+    if(!isset($_SESSION['bibdk_cart']))
+      return false;
+    if (in_array($pid, $_SESSION['bibdk_cart']))
+        return true;
+    return false;
+  }
+
 }
