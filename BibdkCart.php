e8986e0dd711c0075e81f76374e3477aa4efa94d
Martin Møller <mmj@dbc.dk>
2013-02-06 14:57:49 +0100

    Formatting

--- BibdkCart.php
+++ BibdkCart.php
@@ -4,56 +4,68 @@ class BibdkCart {
 
   /**
    * Add pid to bibdk_cart
+   *
    * @param $pids string|array
    */
-  public static function add ($pids){
-    if (!is_array($pids))
+  public static function add($pids) {
+    if (!is_array($pids)) {
       $pids = array($pids);
-    foreach($pids as $pid)
+    }
+    foreach ($pids as $pid) {
       $_SESSION['bibdk_cart'][$pid] = $pid;
+    }
   }
 
   /**
    * Remove pid from cart
+   *
    * @param $pids string|array
    */
-  public static function remove ($pids){
+  public static function remove($pids) {
 
-    if (!is_array($pids))
+    if (!is_array($pids)) {
       $pids = array($pids);
+    }
 
-    foreach($pids as $pid){
-      if (isset($_SESSION['bibdk_cart'][$pid]))
+    foreach ($pids as $pid) {
+      if (isset($_SESSION['bibdk_cart'][$pid])) {
         unset($_SESSION['bibdk_cart'][$pid]);
+      }
     }
   }
 
   /**
    * Get all pids in cart
+   *
    * @return array
    */
-  public static function getAll(){
-    if(!isset($_SESSION['bibdk_cart']))
+  public static function getAll() {
+    if (!isset($_SESSION['bibdk_cart'])) {
       return array();
+    }
     return $_SESSION['bibdk_cart'];
   }
 
-  public static function emptyCart(){
-    if (isset($_SESSION['bibdk_cart']))
+  public static function emptyCart() {
+    if (isset($_SESSION['bibdk_cart'])) {
       unset($_SESSION['bibdk_cart']);
+    }
   }
 
   /**
    * Check if pid is in cart
+   *
    * @param $pid
    * @return bool
    */
-  public static function checkInCart($pid){
-    if(!isset($_SESSION['bibdk_cart']))
-      return false;
-    if (in_array($pid, $_SESSION['bibdk_cart']))
-        return true;
-    return false;
+  public static function checkInCart($pid) {
+    if (!isset($_SESSION['bibdk_cart'])) {
+      return FALSE;
+    }
+    if (in_array($pid, $_SESSION['bibdk_cart'])) {
+      return TRUE;
+    }
+    return FALSE;
   }
 
 }
