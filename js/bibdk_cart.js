52fe7f1ffdca49c35aa9c5dab172694ada6eecf3
Martin Møller <mmj@dbc.dk>
2013-02-07 15:24:08 +0100

    Cart menu item added

--- js/bibdk_cart.js
+++ js/bibdk_cart.js
@@ -1,19 +1,16 @@
 (function ($) {
 
     Drupal.cartResponse = function (data) {
-        console.log(data);
         if (data.saved) {
             $('.link-add-basket[data-pid=' + data.pid + ']').text(Drupal.t('remove_item_from_cart', null, null));
         } else {
             $('.link-add-basket[data-pid=' + data.pid + ']').text(Drupal.t('add_item_to_cart', null, null));
         }
+        $('.cartcount').text(data.cartcount);
     }
 
     Drupal.addRemoveItem = function (element) {
         var pid = $(element).attr('data-pid');
-        /* Add throbber*/
-        //$(element).addClass('ajax-progress');
-        //$(element).html('<span class="throbber">&nbsp;</span>');
 
         var request = $.ajax({
             url:Drupal.settings.basePath + 'cart/ajax',
