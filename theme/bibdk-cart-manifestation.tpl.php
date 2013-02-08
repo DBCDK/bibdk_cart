4dc8931b0bd2f6d0d9bd72c8fd14efd44cfa6237
Søren Vibjerg <vibjerg@gmail.com>
2013-02-08 08:27:31 +0100

    Added template for cart view in manifestation (not finished feature)

---
+++ theme/bibdk-cart-manifestation.tpl.php
@@ -0,0 +1,26 @@
+<?php dpm($variables); ?>
+<article class="manifestation clearfix">
+    <div class="actions">
+
+        <div class="primary-actions">
+            <div class="btn-wrapper">
+              <?php print drupal_render($fields['bibdk_reservation_button_default']); ?>
+            </div>
+        </div>
+        <div class="secondary-actions">
+            <ul>
+              <?php print drupal_render($fields['bibdk_cart_link_default']); ?>
+              <?php print drupal_render($fields['bibdk_linkme_permalink_default']); ?>
+              <?php print drupal_render($fields['bibdk_holdingstatus_favourite_default']); ?>
+            </ul>
+        </div>
+    </div>
+    <!-- .actions -->
+    <div class="manifestation-data text">
+        <table>
+            <tbody>
+            <?php print drupal_render($fields['ting_openformat_default_formatter']); ?>
+            </tbody>
+        </table>
+    </div>
+</article>
