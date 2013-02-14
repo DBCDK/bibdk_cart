d6efd236a5c7fe6b92ac6c6561190aec90d0ffcf
Martin Møller <mmj@dbc.dk>
2013-02-14 15:34:41 +0100

    Cart tpl changes

--- theme/bibdk-cart-manifestation.tpl.php
+++ theme/bibdk-cart-manifestation.tpl.php
@@ -1,5 +1,4 @@
-<article
-        class="element-section manifestation clearfix cart-item-id-<?php print $pid; ?>">
+<article class="cart-item-id-<?php print $pid; ?>">
     <div class="actions">
 
         <div class="primary-actions">
@@ -13,7 +12,6 @@
             </ul>
         </div>
     </div>
-    <!-- .actions -->
     <div class="manifestation-data text">
         <h4><?php print $title; ?></h4>
         <h6><?php print $author; ?></h6>
