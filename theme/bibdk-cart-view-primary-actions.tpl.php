8bb133c4396daa0022ae22e04bb2973045dbb912
Søren Vibjerg <vibjerg@gmail.com>
2013-03-06 17:30:44 +0100

    Styling and refactoring of action buttons

--- theme/bibdk-cart-view-primary-actions.tpl.php
+++ theme/bibdk-cart-view-primary-actions.tpl.php
@@ -1,5 +1,5 @@
-<div class="'cart-view-primary-actions'">
-    <ul>
+<div class="cart-view-primary-actions">
+    <ul class='cart-actions'>
         <li><a href="#" class="cart-view-mark-all"><?php print t('mark_all_none', array(), array('context' => 'bibdk_cart')); ?></a> </li>
         <li><a href="#" class="inactive cart-view-delete-selected"><?php print t('delete_selected', array(), array('context' => 'bibdk_cart')); ?></a> </li>
     </ul>
