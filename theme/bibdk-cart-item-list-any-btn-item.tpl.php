58a5c17ef095cde040eb66aa6127b866254c6e4e
JrgenGNielsen <jgn@dbc.dk>
2013-09-02 15:31:33 +0200

    1094 style "to cart" checkboxes

--- theme/bibdk-cart-item-list-any-btn-item.tpl.php
+++ theme/bibdk-cart-item-list-any-btn-item.tpl.php
@@ -1,4 +1,4 @@
-<a>
+<span class="cart-item">
   <input id="<?php print $check_id; ?>"
          class="cart-checkbox"
          type="checkbox"
@@ -6,4 +6,4 @@
          value="<?php print $pid; ?>"
          data-pid="<?php print $pid; ?>" <?php print $checked; ?>
          />
-</a>
+</span>
