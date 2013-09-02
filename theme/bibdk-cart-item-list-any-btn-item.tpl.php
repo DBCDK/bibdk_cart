4cec74daed26db96a42efd034e4f1cfb3382a9b5
JrgenGNielsen <jgn@dbc.dk>
2013-09-02 14:47:12 +0200

    1094 put arrow in theme function

---
+++ theme/bibdk-cart-item-list-any-btn-item.tpl.php
@@ -0,0 +1,9 @@
+<a>
+  <input id="<?php print $check_id; ?>"
+         class="cart-checkbox"
+         type="checkbox"
+         name="<?php print $name; ?>"
+         value="<?php print $pid; ?>"
+         data-pid="<?php print $pid; ?>" <?php print $checked; ?>
+         />
+</a>
