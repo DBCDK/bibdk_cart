48836d7e0b6a0ebab3a91e4886b2074d7af012d8
Martin Møller <mmj@dbc.dk>
2014-07-24 10:24:48 +0200

    Removed unused template

--- theme/bibdk-cart-item-list-any-btn-item.tpl.php
+++
@@ -1,9 +0,0 @@
-<span class="cart-item">
-  <input id="<?php print $check_id; ?>"
-         class="cart-checkbox"
-         type="checkbox"
-         name="<?php print $name; ?>"
-         value="<?php print $pid; ?>"
-         data-pid="<?php print $pid; ?>" <?php print $checked; ?>
-         />
-</span>
