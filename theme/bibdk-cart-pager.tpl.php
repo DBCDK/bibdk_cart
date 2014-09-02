9321ce13040e7271be3c8cad45bdcce6dd3e5b97
Martin Møller <mmj@dbc.dk>
2014-09-02 14:38:42 +0200

    Minor fixes

--- theme/bibdk-cart-pager.tpl.php
+++ theme/bibdk-cart-pager.tpl.php
@@ -1,9 +1,5 @@
 <div id="bibdk-cart-pager">
-  <?php print $variables['links']['prev']; ?>
-
   <?php foreach ($variables['links']['pages'] as $link): ?>
     <?php print $link; ?>
   <?php endforeach; ?>
-
-  <?php print $variables['links']['next']; ?>
 </div>
